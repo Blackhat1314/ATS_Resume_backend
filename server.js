1084cfc67881f868c0feb95601f4a8b8f88f19f3
Mayank <mayankkaushikofficial@gmail.com>
2025-12-18 00:11:53 +0530

    Chanegs in server

--- server.js
+++ server.js
@@ -1024,8 +1024,8 @@ app.get('/api/health', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on ${PORT}`);
 });
 
 
